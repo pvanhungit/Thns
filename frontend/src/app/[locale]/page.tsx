f9ef497a769242634f46e8d71cd61881992c7f09
Khanh Lam <quockhanhdev@gmail.com>
2024-04-03 20:35:07 +0700

    [fe] Add file .env.example + Remove navigate on home page (#1)

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,7 +1,19 @@
-import { redirect } from 'next/navigation';
+import NextLink from 'next/link';
+
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 // ----------------------------------------------------------------------
 
 export default function HomePage() {
-  return redirect('/auth/sign-in');
+  return (
+    <Stack justifyContent="center" alignItems="center">
+      <Typography>Hello world</Typography>
+
+      <NextLink href="/auth/sign-in">
+        <Button>Login</Button>
+      </NextLink>
+    </Stack>
+  );
 }
