e52f3d2c049df6a4b3c93268fe031bfa850b5588
PhuocLe <levanphuoc141095@gmail.com>
2024-04-03 22:11:43 +0700

    [BACKEND] new module

---
+++ backend/src/main/java/com/example/backend/controller/ControllerLogin.java
@@ -0,0 +1,21 @@
+package com.example.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ControllerLogin{
+    @GetMapping("/person")
+    public List<String> getAllPersons(){
+        //Returns hardcoded data, a real world application would return from the database
+        List<String> personList = new ArrayList<>();
+        personList.add("Hưng");
+        personList.add("Tuấn");
+        personList.add("Phước");
+        personList.add("Khánh");
+        return personList;
+    }
+}
